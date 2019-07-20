46229a12250d84821307e5050cf776d2ab7e5dfc
santhoshclement <53114856+santhoshclement@users.noreply.github.com>
2019-07-20 23:02:31 +0530

    only disable and move to Deprovison OU
    # This will Disable , Move to OU , Add Descriptio for set of Accounts

--- MonthlyAudit.ps1
+++ MonthlyAudit.ps1
@@ -11,3 +11,14 @@ Get-ADUser $user -Properties * | select samaccountname,enabled,DistinguishedName
 
 #############################################################################
 
+
+# This will Disable , Move to OU , Add Descriptio for set of Accounts 
+$users= get-content C:\temp\SNAME2.txt 
+foreach($user in $users)
+{
+
+
+Set-ADuser -Identity $user -Enabled $false
+Get-ADUser $user | Move-ADObject -TargetPath "OU=Deprovisioned,OU=User Accounts,OU=MSP01,DC=ten,DC=thomsonreuters,DC=com" -PassThru
+Set-ADUser $user -Description "Do not re-enable the account*leaver account*"  
+}
